8f84c150bce89602270d774e048f9baaebc4056b
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-10-30 21:30:48 +0800

    Update Bonezegei_WS2812.h

--- src/Bonezegei_WS2812.h
+++ src/Bonezegei_WS2812.h
@@ -1,1 +1,20 @@
+/*
+  This Library is written for WS2812
+  Author: Bonezegei (Jofel Batutay)
+  Date : October 2023
+  RGB LED Controller
+*/
+#include "esp32-hal.h"
 
+#ifndef _BONEZEGEI_WS2812_H_
+#define _BONEZEGEI_WS2812_H_
+class Bonezegei_WS2812 {
+public:
+  Bonezegei_WS2812();
+
+  rmt_data_t *led_data;
+  int _pin;
+  int led_count;
+}
+
+#endif
