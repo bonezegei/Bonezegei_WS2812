6f95418bfa69c1ec1fed6e02fa575aeb71214c37
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-10-30 23:18:00 +0800

    Update Bonezegei_WS2812.h

--- src/Bonezegei_WS2812.h
+++ src/Bonezegei_WS2812.h
@@ -1,20 +1,46 @@
-/*
+*
   This Library is written for WS2812
   Author: Bonezegei (Jofel Batutay)
   Date : October 2023
   RGB LED Controller
 */
+
 #include "esp32-hal.h"
+#include <Arduino.h>
 
 #ifndef _BONEZEGEI_WS2812_H_
 #define _BONEZEGEI_WS2812_H_
+
 class Bonezegei_WS2812 {
 public:
-  Bonezegei_WS2812();
+  //1 LED
+  Bonezegei_WS2812(int pin);
+  //Multiple LED in single strip
+  Bonezegei_WS2812(int pin, int led_count);
+  //Multiple LED in Matrix
+  Bonezegei_WS2812(int pin, int row, int col);
 
-  rmt_data_t *led_data;
+  //Initialize
+  int begin();
+
+  void setPixel(uint32_t color);
+
+  void setPixel(int index, uint32_t color);
+
+  void clear();
+
+  //fill all LED 
+  void fill(uint32_t color);
+
+  rmt_obj_t* rmt_send = NULL;
+
+  rmt_data_t* led_data;
+  
   int _pin;
-  int led_count;
-}
+  int _row;
+  int _col;
+  int _led_count;
+  int _led_rmt;
+};
 
 #endif
